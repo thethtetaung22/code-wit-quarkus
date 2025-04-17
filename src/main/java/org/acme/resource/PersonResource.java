17b6cc0dbdc4c0c6db30cb66a25e536e2ae51e6a
Thet Htet Aung <thethtetaung17@gmail.com>
2025-04-17 19:41:56 +0800

    Update and delete api

--- src/main/java/org/acme/resource/PersonResource.java
+++ src/main/java/org/acme/resource/PersonResource.java
@@ -7,11 +7,9 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.acme.domain.Person;
 import org.acme.model.PersonModel;
 import org.acme.service.PersonService;
-
-import java.util.List;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 @Path("/persons")
 @Produces(MediaType.APPLICATION_JSON)
@@ -22,9 +20,29 @@ public class PersonResource {
         return Response.ok(PersonService.getPersons()).build();
     }
 
+    @GET
+    @Path("{id}")
+    public Response getPersonById(@PathParam Long id) {
+        return Response.ok(PersonService.getPersonById(id)).build();
+    }
+
     @POST
     @Transactional
     public Response createPerson(@NotNull PersonModel personModel) {
         return Response.ok(PersonService.createPerson(personModel)).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response updatePerson(@PathParam Long id, @NotNull PersonModel personModel) {
+        return Response.ok(PersonService.updatePerson(id, personModel)).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deletePersonById(@PathParam Long id) {
+        return Response.ok(PersonService.deletePersonById(id)).build();
     }
 }
--- src/main/java/org/acme/service/PersonService.java
+++ src/main/java/org/acme/service/PersonService.java
@@ -1,6 +1,7 @@
 package org.acme.service;
 
 import io.smallrye.common.constraint.NotNull;
+import jakarta.ws.rs.WebApplicationException;
 import org.acme.domain.Person;
 import org.acme.model.PersonModel;
 
@@ -12,9 +13,36 @@ public class PersonService {
         return Person.findAll().list();
     }
 
+    public static Person getPersonById(Long id) {
+        return Person.findById(id);
+    }
+
     public static Person createPerson(@NotNull PersonModel model) {
         Person person = new Person(model.name, model.age);
         person.persist();
         return person;
+    }
+
+    public static Person updatePerson(Long id, @NotNull PersonModel model) {
+        Person person = Person.findById(id);
+
+        if (person == null) {
+            throw new WebApplicationException("Person with this id does not exist!", 404);
+        }
+        person.name = model.name;
+        person.age = model.age;
+        person.persist();
+        return person;
+    }
+
+    public static String deletePersonById(Long id) {
+        Person person = Person.findById(id);
+
+        if (person == null) {
+            throw new WebApplicationException("Person with this id does not exist!", 404);
+        }
+
+        person.delete();
+        return "Person with this ID deleted successful!";
     }
 }
